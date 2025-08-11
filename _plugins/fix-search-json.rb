e30030aed22be70ed0e5b31e422293f2b1d48596
Jauved <tfwditr@hotmail.com>
2025-08-12 02:33:32 +0800

    fixed: 修正\b不会被替换的问题.

--- _plugins/fix-search-json.rb
+++ _plugins/fix-search-json.rb
@@ -20,12 +20,11 @@ Jekyll::Hooks.register :site, :post_write do |site|
 
   # 匹配单个反斜杠，后面不是 转义字符 或 JSON 专用转义
   # slash_pattern = /\\(?=[^\\\/\"bfnrtu])/
-  slash_pattern = /\\(?!\\)/
+  slash_pattern = /\\(?=[^\\\/\"fnrtu])/
 
   # 替换操作：Tab 转空格，未转义的反斜杠加转义
   new_text = text.gsub("\t", "    ")
-  # new_text = new_text.gsub(slash_pattern) { "\\\\" }
-  new_text = new_text.gsub(slash_pattern) { "\\\\\\\\" }
+  new_text = new_text.gsub(slash_pattern) { "\\\\" }
 
   # 写入并验证
   File.open(path, 'wb') { |f| f.write(new_text) }
