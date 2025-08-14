f4ab93aa8691fe6ec5fdc54bb822bf5d4f98fa89
Jauved <tfwditr@hotmail.com>
2025-08-14 14:12:35 +0800

    fixed: 将修改范围最小化到content的内容

--- _plugins/fix-search-json.rb
+++ _plugins/fix-search-json.rb
@@ -21,12 +21,18 @@ Jekyll::Hooks.register :site, :post_write do |site|
   # 匹配单个反斜杠，后面不是 转义字符 或 JSON 专用转义
   # slash_pattern = /\\(?=[^\\\/\"bfnrtu])/
   # slash_pattern = /\\(?=[^\\\/\"fnrtu])/
-  slash_pattern = /\\/
 
   # 替换操作：Tab 转空格，未转义的反斜杠加转义
   new_text = text.gsub("\t", "    ")
   # new_text = new_text.gsub(slash_pattern) { "\\\\" }
-  new_text = new_text.gsub(slash_pattern) { "" }
+  # new_text = new_text.gsub(slash_pattern) { "" }
+  new_text = new_text.gsub(/("content"\s*:\s*")((?:[^"\\]|\\.)*?)(")/) do
+    key = Regexp.last_match(1)  # "content":
+    val = Regexp.last_match(2)  # 内容
+    endq = Regexp.last_match(3) # 结束引号
+    cleaned_val = val.gsub("\\", "")
+    "#{key}#{cleaned_val}#{endq}"
+  end
 
   # 写入并验证
   File.open(path, 'wb') { |f| f.write(new_text) }
